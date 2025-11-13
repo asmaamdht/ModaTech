f80abdb24136c363069827198fe9052f1808c8eb
Asmaa Medhat <136195857+asmaamdht@users.noreply.github.com>
2025-11-13 20:52:32 +0200

    Create UserProfile, Custom Bottom,Input,Title

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text, StyleSheet
+} from "react-native";
 
-const StyledText = () => {
+type StyleTextType = {
+  title: string,
+}
+const StyledText: React.FC<StyleTextType> = ({ title }) => {
   return (
-    <View>
-      <Text>StyledText</Text>
-    </View>
+    <Text style={StyleTitle.title}>{title}</Text>
   );
 };
 
 export default StyledText;
+
+
+
+const StyleTitle = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: '#ffffffff',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '400',
+    marginBottom: 35,
+  },
+
+});
+
+
---
+++ src/features/Profile/StyleProfile.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const StyleProfile = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+        backgroundColor: '#ffffffff',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '400',
+        marginBottom: 35,
+    },
+
+});
+
+
+export default StyleProfile;
