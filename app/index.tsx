d583880f02f6213bce534fc3a1897a44443a18eb
Asmaa Medhat <136195857+asmaamdht@users.noreply.github.com>
2025-11-15 23:10:53 +0200

    UpdateRout & HandleLogout

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,17 @@
+import { store } from "@/src/redux/store";
 import Router from "@/src/router/Router";
 import React from "react";
+import { Provider } from "react-redux";
 
 const index = () => {
-  return <Router />;
+
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  )
+
+
 };
 
 export default index;
